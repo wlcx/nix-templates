85999ed2cfe5bbbdf59209e4dd2c2c98989115ba
Sam Willcocks <sam@wlcx.cc>
2022-05-26 23:43:22 +0100

    Add basic devshell template

---
+++ devshell/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A basic flake from samw";
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
+  outputs = { self, nixpkgs, utils, devshell }:
+    utils.lib.eachDefaultSystem (system: {
+      devShell = let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlay ];
+        };
+      in pkgs.devshell.mkShell { packages = with pkgs; [ ]; };
+    });
+}
